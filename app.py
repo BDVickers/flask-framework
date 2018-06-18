886b85e1b62dbafbf906bb82d748302b131a7929
Brian Donald Vickers <brvicker@mac-ekross06.local>
2018-06-18 11:04:50 -0400

    New input from

--- app.py
+++ app.py
@@ -2,9 +2,29 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+app.vars = {}
+
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-  return render_template('index.html')
+	default_stock = 'GOOG'
+	if request.method == 'GET':
+		return render_template('figure.html', stock_id = default_stock)
+  else:
+  	# request was a POST
+  	app.vars['stock'] = request.form['stock_id']
+
+  	f = open('%s.txt'%(app.vars['stock']), 'w')
+  	f.write('Stock: %s\n'%(app.vars['stock']))
+  	f.close()
+
+  	return render_template('figure.html', stock_id = default_stock)
+
+
+  	return render_template('index.html')
+
+@app.route('/figure')
+def figure():
+	return render_template('figure.html')
 
 @app.route('/about')
 def about():
